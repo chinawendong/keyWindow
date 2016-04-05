9fd71562ff4547ab575b90939049bd8c954ceac0
chinawendong <790175706@qq.com>
2016-04-05 14:48:51 +0800

    Initial Commit

---
+++ keyWindow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  keyWindow
+//
+//  Created by 云族佳 on 16/4/5.
+//  Copyright © 2016年 云族佳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ keyWindow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  keyWindow
+//
+//  Created by 云族佳 on 16/4/5.
+//  Copyright © 2016年 云族佳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
